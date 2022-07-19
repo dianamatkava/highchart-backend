041116c0b0797ae6d672aec1c8d65074c0d705fd
dianaGera <88348384+dianaGera@users.noreply.github.com>
2022-07-19 11:40:17 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -123,10 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-STATICFILES_STORAGE = 'backend.storage.S3Storage'
 
 
 MEDIA_URL = '/media/'
