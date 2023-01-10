74489f8aadee940ffa44fafb7259f25b53bae324
dianaGera <dianamatkava@gmail.com>
2023-01-10 13:32:32 +0100

    Updated charts

--- personal_report/views.py
+++ personal_report/views.py
@@ -20,7 +20,7 @@ def import_rpt(request):
         except Exception as _ex:
             print(_ex)
         return cohort_rpt(request)
-            
+
     return render(request, 'import_rpt.html')
 
 
@@ -28,24 +28,24 @@ def round_score(num:int(), truncate:int(), persentage:bool()):
             if persentage:
                 return round(round(num++1e-06, truncate)*100)
             return round(num++1e-06, truncate)
-        
-        
+
+
 def truncate_items(data:dict(), table_list:list()):
     # import pdb;pdb.set_trace()
     for table in table_list:
         if len(data[table]['LEARNERS']) > 10:
             x = data[table]['LEARNERS'][len(data[table]['LEARNERS'])-1]
             other = 0
-            
-            for i in range(len(data[table]['LEARNERS']), 0, -1): 
+
+            for i in range(len(data[table]['LEARNERS']), 0, -1):
                 if x!= data[table]['LEARNERS'][i-1]:
                     if i-1 < 9:
                         data[table]['LEARNERS'] = data[table]['LEARNERS'][0:i]
                         data[table]['LEARNERS'].append(other)
-                        
+
                         data[table][table] = data[table][table][0:len(data[table]['LEARNERS'])-1]
                         data[table][table].append('Other')
-                        
+
                         # Truncate dict
                         del_key = list()
                         for key in data[table].keys():
@@ -53,7 +53,7 @@ def truncate_items(data:dict(), table_list:list()):
                                 del_key.append(key)
                         list(map(data[table].__delitem__, filter(data[table].__contains__,del_key)))
                         break
-                    
+
                     x = data[table]['LEARNERS'][i-1]
                 other += int(data[table]['LEARNERS'][i-1])
     return data
@@ -64,46 +64,46 @@ def generate_data(request, file=None):
 
     x = int()
     if request.method == 'POST':
-    
+
         data = defaultdict(lambda: defaultdict(dict))
         filepath = r'media'
         filename = request.session.get('filename')
 
         xls = pd.ExcelFile(os.path.join(settings.BASE_DIR, filepath, filename))
         pages = {f'pg{num}': pd.read_excel(xls, f'pg{num}') for num in range(1, 6)}
-        
+
         table_title, keys_list = str(), list()
         values_list, persentage =[[]]*10, int()
         for page_name, page, in pages.items():
             for row in page.iterrows():
-                if not pd.isna(row[1][0]):   
-                    # Define table title and set up new dict     
-                    print(values_list)            
+                if not pd.isna(row[1][0]):
+                    # Define table title and set up new dict
+                    print(values_list)
                     if type(row[1][0])==str and row[1][0][0:3].startswith(('>>>', '<<<')):
                         table_title = '_'.join(row[1][0].split()[1::]).upper()
                         persentage = 1 if str(row[1][1]) == '%' else 0
                         data[table_title] = dict()
-                        
+
                     # Defines if wether we are looping throught headers
-                    elif len(row[1]) < 4 and type(row[1][1]) == str: 
+                    elif len(row[1]) < 4 and type(row[1][1]) == str:
                         keys_list = '_'.join(str(row[1][0]).split()).upper()
                         if len(row[1]) < 4:
                             values_list[0] = '_'.join(row[1][1].split()).upper()
                             if len(row[1]) == 3 and type(row[1][2]) == str:
                                 values_list[1] = '_'.join(row[1][2].split()).upper()
-                                
-                    # Defines if wether we are looping throught headers on page 3/4    
+
+                    # Defines if wether we are looping throught headers on page 3/4
                     elif len(row[1]) > 3 and type(row[1][3]) == str:
                         print('\n', row[1][0], row[1][1], row[0])
                         keys_list = '_'.join(str(row[1][0]).split()).upper()
                         for i, header in enumerate(row[1]):
                             values_list[i] = '_'.join(header.split()).upper()
-                            
+
                     elif len(row[1]) > 3 and type(row[1][1]) == str and page_name=='pg3':
                         values_list[0] = '_'.join(row[1][1].split()).upper()
                         values_list[1] = '_'.join(row[1][0].split()).upper()
-                        
-                    # Looping through main table content (values). 
+
+                    # Looping through main table content (values).
                     elif len(row[1]) < 4 or page_name == 'pg3' and type(row[1][1]) in [int, float]:
                         data[table_title][row[1][0]] = round_score(row[1][1], 2, persentage)
                         data[table_title].setdefault(keys_list, []).append(row[1][0])
@@ -114,23 +114,23 @@ def generate_data(request, file=None):
                                     data[table_title].setdefault(values_list[i+1], []).append(0)
                                 else:
                                     data[table_title].setdefault(values_list[i+1], []).append(round_score(row[1][i+1], 2, persentage))
-                            
+
                     # Looping through main table content (values) on page 4
                     elif page_name == 'pg4':
                         data[table_title].setdefault(keys_list, []).append(row[1][0])
                         data[table_title].setdefault(row[1][1], {})
                         for idx in range(2, 5):
                             data[table_title][row[1][1]].setdefault(values_list[idx], []).append(round_score(row[1][idx], 2, persentage))
-                        
+
             # Remove duplicates from Module/Stage (page4)
             data['GAP_TO_GOAL']['MODULE/STAGE'] = list(dict.fromkeys(data['GAP_TO_GOAL']['MODULE/STAGE']))
             data['SCORE_BY_MODULE']['MODULE'] = list(filter(lambda i: not isinstance(i, int), data['SCORE_BY_MODULE']['MODULE']))
         # Merge xtra items (more the 10) to "Other"
         data = truncate_items(data, ['BUSINESS_UNIT', 'LOCATION', 'EXPERTISE'])
-        
-        data_obj = json.dumps(data, indent = 4) 
+
+        data_obj = json.dumps(data, indent = 4)
         del request.session['filename']
-        
+
         print(data_obj)
         return JsonResponse(data_obj, safe=False)
 
